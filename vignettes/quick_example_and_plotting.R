d96c76f8cd009d8659b4963eeaa3e787f0a8aad2
michaelchimento <mchimento@ab.mpg.de>
2024-12-17 22:33:56 +0100

    forgot pic

---
+++ vignettes/quick_example_and_plotting.R
@@ -0,0 +1,51 @@
+library(STbayes)
+diffusion_data = STbayes::example_diffusion_data
+edge_list = STbayes::example_edge_list
+
+#generate STAN model from input data
+data_list_user = import_user_STb(diffusion_data, edge_list)
+
+#generate STAN model from input data
+model_obj = generate_STb_model(data_list_user, gq=T, est_acqTime = T)
+
+# fit model
+fit = fit_STb(data_list_user, model_obj, chains = 5, cores = 5, iter=2000, control = list(adapt_delta=0.99))
+
+# check estimates
+STb_summary(fit, digits=4)
+
+#get data for estimated times
+acqdata = extract_acqTime(fit, data_list_user)
+
+#plot estimated times versus observed times w/ residuals
+ggplot(acqdata, aes(x = observed_time, y = mean_time)) +
+    geom_segment(
+        aes(x = observed_time, xend = observed_time, y = mean_time, yend = observed_time), # connect predicted to slope line
+        color = "red",
+        alpha = 0.2
+    ) +
+    geom_point(alpha = 0.6, size=2) +
+    geom_abline(intercept = 0, slope = 1, color = "black", linetype = "dashed") +
+    facet_wrap(~trial, scales = "free_x") +
+    labs(
+        title = "Estimated acquisition time with residuals",
+        x = "Observed time",
+        y = "Estimated time"
+    ) +
+    theme_minimal()
+
+
+#plot estimated times versus observed times w/ HPD
+ggplot(acqdata, aes(x = observed_time, y = mean_time)) +
+    geom_abline(slope = 1, intercept = 0,             # Line y = x
+                color = "red", linetype = "dotted", size = 1) +
+    geom_pointrange(aes(ymin = lower_hpd, ymax = upper_hpd), size=.8) +
+    facet_wrap(~trial, scales = "free_x") +
+    labs(
+        title = "Estimated acquisition time with 95% HPD Intervals",
+        x = "Observed time",
+        y = "Estimated time"
+    ) +
+    theme_minimal()
+
+ggsave("../data/estimates_residuals.png", width=8, height=8, units="cm", scale=1.5)
