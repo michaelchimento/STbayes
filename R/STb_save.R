1558bf4b144582a44a135db913cf5592ffde292b
michaelchimento <mchimento@ab.mpg.de>
2025-04-25 15:47:32 +0200

    update save func so rds includes data

--- R/STb_save.R
+++ R/STb_save.R
@@ -1,33 +1,38 @@
 #' Save STbayes objects easily
 #'
 #' @param fit model fitted by STbayes
+#' @param output_dir where do you want to save it???
 #' @param name optional name to use for model
-#' @param path where do you want to save it???
 #'
 #' @return
 #' @export
 #'
 #' @examples
-STb_save <- function(fit, name = NULL, path = "cmdstan_saves") {
+STb_save <- function(fit, output_dir = "cmdstan_saves",  name = NULL) {
     # infer  name if not provided
     if (is.null(name)) {
         name <- deparse(substitute(fit))
     }
+    #just get rid for nice printing later
+    output_dir = sub("/+$", "", output_dir)
+
+    full_path <- file.path(output_dir)
 
     # create dir
-    dir.create(path, showWarnings = FALSE, recursive = TRUE)
+    dir.create(full_path, showWarnings = FALSE, recursive = TRUE)
 
     # generate CSV file names
-    csv_files <- file.path(path, paste0(name, "_chain_", seq_along(fit$output_files()), ".csv"))
+    csv_files <- file.path(full_path, paste0(name, "_chain_", seq_along(fit$output_files()), ".csv"))
 
     # copy CSVs
-    file.copy(fit$output_files(), to = csv_files, overwrite = TRUE)
+    #file.copy(fit$output_files(), to = csv_files, overwrite = TRUE)
 
     # update the fit object to use new file paths
-    fit$run_set$output_files_ <- csv_files
+    fit_newfiles = cmdstanr::as_cmdstan_fit(fit$output_files())
 
     # save the R object
-    saveRDS(fit, file = file.path(path, paste0(name, ".rds")))
+    saveRDS(fit_newfiles, file = file.path(full_path, paste0(name, ".rds")))
 
-    message(paste("Fit + chains successfully saved in", path))
+    message(paste0("Fit & chains successfully saved."))
+    message(paste0("You can load fit again with readRDS('", full_path,"/",name,".rds')"))
 }
