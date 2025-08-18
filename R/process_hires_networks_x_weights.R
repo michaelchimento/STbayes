04c85b01808218c348852c6a55bcedc7a8648006
michael <mchimento@ab.mpg.de>
2025-08-18 17:10:56 +0200

    fixed bug in process_networks_x_weights, from-to focal-other issue

--- R/process_hires_networks_x_weights.R
+++ R/process_hires_networks_x_weights.R
@@ -1,7 +1,7 @@
 utils::globalVariables(c(
     ":=", ".SD", ".",
     "t_weight", "trial", "time", "duration",
-    "from", "to", "discrete_time"
+    "focal", "other", "discrete_time"
 ))
 #' process_networks_x_weights_hires()
 #'
@@ -19,7 +19,7 @@ process_networks_x_weights_hires <- function(event_data, t_weights, networks, D_
     t_weights$trial <- as.integer(as.factor(t_weights$trial))
     t_weights$id <- as.integer(as.factor(t_weights$id))
 
-    names(t_weights)[names(t_weights) == "id"] <- "to"
+    names(t_weights)[names(t_weights) == "id"] <- "other"
     names(D_data)[names(D_data) == "trial_numeric"] <- "trial"
     # join with networks
 
